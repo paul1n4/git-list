d14baf3554ed8b3418183b0db8890d9d36d62284
paul1n4 <paulinevaleroc@gmail.com>
2023-03-07 18:16:11 -0600

    Install react-router-dom, create paths on index.js & add params in App.js

--- src/App.js
+++ src/App.js
@@ -3,28 +3,21 @@ import Layout from './components/Layout';
 import Profile from './components/Profile';
 import RepoList from './components/Repo-list';
 import Search from './components/Search';
-import repoData from './components/repo-data'
 import { useState, useEffect } from 'react'
 import { getUser, getRepos } from './services/users';
-
-
-/* const repoList = [
-  {
-    name: 'mi primer proyecto react', 
-    id: 123,
-  },
-  {
-    name: 'mi segundo proyecto react', 
-    id: 124,
-  }
-] */
+import { useParams } from 'react-router-dom'
 
 function App() {
+  const params = useParams()
+  let username = params.user
+  if(!username) {
+    username = 'leonidasesteban'
+  }
   const [user, setUser] = useState([])
   const [repos, setRepos] = useState([])
 
   useEffect(() => {
-    getUser('leonidasesteban').then(({ data, isError}) => {
+    getUser(username).then(({ data, isError}) => {
       if(isError){
         console.log('No hemos encontrado a este crack');
         return
@@ -34,7 +27,7 @@ function App() {
   }, [])
   
   useEffect(() => {
-    getRepos('leonidasesteban').then(({ data, isError}) => {
+    getRepos(username).then(({ data, isError}) => {
       if(isError){
         console.log('No hemos encontrado los repos de este crack');
         return
--- src/index.js
+++ src/index.js
@@ -3,13 +3,25 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import GlobalStylesStyled from './components/Global-styles';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider} from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />
+  },
+  {
+    path: '/:user',
+    element: <App />
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <>
       <GlobalStylesStyled />
-      <App />
+      <RouterProvider router={router}/> 
     </>
   </React.StrictMode>
 );
