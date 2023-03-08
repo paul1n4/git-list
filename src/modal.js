9e5097a460a9a3f1b7a2a90be605c23da20e5ee1
paul1n4 <paulinevaleroc@gmail.com>
2023-03-07 21:21:57 -0600

    Add styles for modal, create Overlay and Input-text components

--- src/modal.js
+++ src/modal.js
@@ -1,5 +1,29 @@
 import { useRef } from 'react'
 import { useNavigate } from 'react-router-dom'
+import Overlay from './Overlay'
+import styled from 'styled-components'
+import { ButtonContrast } from './components/Button'
+import InputText from './components/Input-text'
+
+const ModalContentStyled = styled.form`
+  background: var(--bg);
+  color: var(--white);
+  padding: 1.5rem;
+  border-radius: .5rem;
+  position: fixed;
+  inset-block-start: 50%;
+  transform: translateY(-50%);
+  inset-inline-start: 50%;
+  transform: translateX(-50%);
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  inline-size: 24rem;
+  .title {
+    font: var(--headline2-semi-bold);
+    margin: 0;
+  }
+`
 
 function ModalContent() {
   const form = useRef(null)
@@ -13,11 +37,13 @@ function ModalContent() {
   }
 
   return (
-    <form ref={form} action="" onSubmit={(handleSubmit)}>
-      <h2>Search any user</h2>
-     <input type="text" name="username" placeholder="Username"/> 
-     <button>Buscar</button>
-    </form>
+    <Overlay>
+      <ModalContentStyled ref={form} action="" onSubmit={(handleSubmit)}>
+        <h2 className='title'>Search any user</h2>
+        <InputText type="text" autoComplete='off' name="username" placeholder="Username"/> 
+        <ButtonContrast text="Buscar"/>
+      </ModalContentStyled>
+    </Overlay>
   )
 }
 
